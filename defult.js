3d8daa7643cc47445aa3ba744973734a436acf86
AliBahaa <alibahaa963@gmail.com>
2024-10-09 18:57:57 +0300

    delet task

--- defult.js
+++ defult.js
@@ -9,5 +9,17 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span)
     }
+    inputBox.value = ""
 }
+
+list.addEventListener("click", function (e) {
+    if (e.target.tagName == "LI") {
+        e.target.classList.toggle("chacked")
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+    }
+}, false)
